521458003d44b481729e3ff71c3ad8eaf43f9435
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-09-25 14:27:50 -0400

    Google places and places near me api's all working correctly, results currently logged to console

--- src/api/places.js
+++ src/api/places.js
@@ -1,19 +1,21 @@
-import {Client, Status, axiosInstance, PlaceInputType} from "@googlemaps/google-maps-services-js";
+import {Client, axiosInstance, PlaceInputType} from "@googlemaps/google-maps-services-js";
 
 const client = new Client({});
  
 export default client
   .findPlaceFromText({
     params: {
-    input: 'starbucks',
+    input: 'dunkin donuts',
     inputtype: PlaceInputType.textQuery,
     key: 'AIzaSyBBYO5GUhGzV2Jo0mCE5bYzIHbpLfKC5eo',
-    fields: ["name","place_id","formatted_address", "photos"],
+    fields: ["name","place_id","formatted_address", "photos", "type"],
     },
-    timeout: 1000 // milliseconds
+    timeout: 1000 
   }, axiosInstance)
   .then(r => {
-    console.log('Based on the search name or place_id we have returned a result of: ', r.data.candidates[0]);
+    console.log('Based on the search name or place_id we have returned a result of: ', r.data.candidates[0]
+    , '\n', r.data.candidates);
+
   })
   .catch(e => {
     console.log(process.env)
