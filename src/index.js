162c1a47058a3ae946d29b6840b06a9c8d45e660
ivanzapatarivera <falenyo@gmail.com>
2020-09-29 12:59:21 -0400

    created selections.js front end module

--- src/index.js
+++ src/index.js
@@ -9,9 +9,16 @@ import placesfromlocation from './utils/locationapi/placesfromlocation'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-    {/* <Route exact path="/" component={places} /> */}
-    </Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/welcome" component={Welcome} />
+      <Route exact path="/selections" component={Selections} />
+      <Route exact path="/promo" component={Promotions} />
+      <Route exact path="/locations" component={Locations} />
+      <Route exact path="/order" component={Order} />
+      <Route exact path="/checkout" component={CheckOut} />
+      <Route exact path="/logout" component={Home} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
