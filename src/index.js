cd77c92fb6c10c4bde059b403a6e634a488fead0
ivanzapatarivera <falenyo@gmail.com>
2020-09-29 14:11:27 -0400

    added switch tag

--- src/index.js
+++ src/index.js
@@ -17,16 +17,19 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/welcome" component={Welcome} />
-        <Route exact path="/selections" component={Selections} />
-        <Route exact path="/promo" component={Promotions} />
-        <Route exact path="/locations" component={Locations} />
-        <Route exact path="/order" component={Order} />
-        <Route exact path="/checkout" component={CheckOut} />
-        <Route exact path="/logout" component={Home} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/welcome" component={Welcome} />
+          <Route exact path="/selections" component={Selections} />
+          <Route exact path="/promo" component={Promotions} />
+          <Route exact path="/locations" component={Locations} />
+          <Route exact path="/order" component={Order} />
+          <Route exact path="/checkout" component={CheckOut} />
+          <Route exact path="/logout" component={Home} />
+        </Switch>
+       
       </div>
     </Router>
       
