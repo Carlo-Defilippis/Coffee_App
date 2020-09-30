01bad5d738eb4374572fe20ca5a8d932bd2ae37d
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-09-30 13:38:49 -0400

    merge corrected

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,24 +10,47 @@ import {
   FaLinkedin
 } from 'react-icons/fa';
 import { SiCoffeescript } from 'react-icons/si';
-
 function Footer() {
   return (
     <div className='footer-container'>
-     
       <section className='social-media'>
+        <div className='social-media-wrap'>
           <div className='footer-logo'>
             <Link to='/' className='social-logo'>
               <SiCoffeescript className='navbar-icon' />
               Coffee App
             </Link>
+            </div>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <FaTwitter />
+            </Link>
           </div>
-
+        </div>
       </section>
       <small className='website-rights'>Coffee App © 2020</small>
     </div>
   );
 }
-
 export default Footer;
-
