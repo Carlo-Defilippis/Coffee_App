7ea528f71115de08d397af592e09f9ca08c03990
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-01 13:28:24 -0400

    fixed the naming issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Coffeeshop',
+  process.env.MONGODB_URL || 'mongodb://localhost/Coffeeshop',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
