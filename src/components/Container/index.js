5457419f3a8ad409af313c77ba14829375400b58
Owner <owner@Owners-iMac.local>
2020-10-01 11:31:10 -0400

    all  connected

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 
 function Container(props) {
-  console.log("Props ", props.children)
   return <div className="container" style={props.style}>{props.children}</div>;
 }
 
