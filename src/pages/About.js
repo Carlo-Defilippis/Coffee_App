4ba68a4c1dae5522d16dcab0048e84c9f6c7b4cc
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-10-01 11:44:49 -0400

    index changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,28 +12,51 @@ export default function About() {
   return (
     <div>
       <Navbar>
+
       </Navbar>
+      
       <Container style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%,-50%)"}}>
+      
         <Row>
+      
           <Col size="md-12">
+      
             <h1 style={{ textAlign: "center" }}>Welcome To Coffee App!</h1>
+      
           </Col>
+      
         </Row>
+      
         <Row>
+      
           <Col size="md-4"><div><br></br></div></Col>
+      
           <Col size="md-4">
+      
             <Button>Sign Up</Button>
+      
           </Col>
+      
           <Col size="md-4"><div><br></br></div></Col>
+      
         </Row>
+      
         <Row>
+      
         <Col size="md-4"><div><br></br></div></Col>
+      
           <Col size="md-4">
+      
             <p style={{fontSize: "small", textAlign: "center"}}>If you already have an account <a href="/Login">Log in here.</a>
+      
             </p>
+      
           </Col>
+      
           <Col size="md-4"><div><br></br></div></Col>
+      
         </Row>
+      
       </Container>
     </div>
   );
