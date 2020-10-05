14c31f1a330ffe40d302710e22848927046e558a
Owner <owner@Owners-iMac.local>
2020-10-05 10:02:19 -0400

    final stages

--- src/components/Locations/index.js
+++ src/components/Locations/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import MyComponents from '../utils/locationapi/googlemaps.js'
+import MyComponents from '../../utils/locationapi/googlemaps'
 
 function Google() {
 
