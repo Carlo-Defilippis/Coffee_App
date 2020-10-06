5e962651148f4732be0ae22a7db53d6d8a7db0cd
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-10-06 00:16:03 -0400

    moved jwt token to server.js 5

--- backend/jwt.js
+++ backend/jwt.js
@@ -2,32 +2,32 @@ const expressJwt = require('express-jwt');
 const config = require('../config.json');
 const userService = require('../models/user.service');
 
-// module.exports = jwt;
 
 
-// function jwt() {
-//     var secret = process.env.SECRET
-//     console.log("This is the secret shhhhhh: ",secret)
-//     return expressJwt({ secret, algorithms: ['HS256'], isRevoked }).unless({
-//         path: [
-//             // public routes that don't require authentication
-//             '/',
-//             '/login',
-//             '/register',
-//             '/users/authenticate',
-//             '/users/register'
-//         ]
-//     });
-// }
+function jwt() {
+    var secret = process.env.SECRET
+    console.log("This is the secret shhhhhh: ",secret)
+    return expressJwt({ secret, algorithms: ['HS256'], isRevoked }).unless({
+        path: [
+            // public routes that don't require authentication
+            'https://coffee-app-group6.herokuapp.com/',
+            'https://coffee-app-group6.herokuapp.com/login',
+            'https://coffee-app-group6.herokuapp.com/register',
+            'https://coffee-app-group6.herokuapp.com/users/authenticate',
+            'https://coffee-app-group6.herokuapp.com/users/register'
+        ]
+    });
+}
 
-// async function isRevoked(req, payload, done) {
-//     const user = await userService.getById(payload.sub);
+async function isRevoked(req, payload, done) {
+    const user = await userService.getById(payload.sub);
     
-//     // revoke token if user no longer exists
-//     if (!user) {
-//         console.log('You are not a user!')
-//         return done(null, true);
-//     }
+    // revoke token if user no longer exists
+    if (!user) {
+        console.log('You are not a user!')
+        return done(null, true);
+    }
     
-//     done();
-// };
+    done();
+};
+module.exports = jwt;
